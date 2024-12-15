fc6bb677a93e1d6adf73491fd8d43134505ef6fa
TheDream3r <patrick.raum@gmail.com>
2024-12-15 22:14:35 +0100

    new dist?

--- dist/assets/AboutView-BzPYI7Td.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,a as s,o as n}from"./index-C-Hs8oA3.js";const a={},r={class:"about"};function c(i,e){return n(),t("div",r,e[0]||(e[0]=[s("h1",null,"Hello, my name is dreamer and im trying some things out",-1)]))}const m=o(a,[["render",c]]);export{m as default};
