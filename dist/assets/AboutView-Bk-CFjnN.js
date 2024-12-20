bd8fc8aa3556cdb518c4a03000609e7430b252d7
TheDream3r <patrick.raum@gmail.com>
2024-12-20 16:15:42 +0100

    New build

---
+++ dist/assets/AboutView-Bk-CFjnN.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as t,a as s,o as r}from"./index-Barj-yl7.js";const n={},a={class:"about"};function c(i,e){return r(),t("div",a,e[0]||(e[0]=[s("h1",null,"Hello, my name is dreamer and im trying some things out",-1)]))}const _=o(n,[["render",c]]);export{_ as default};
