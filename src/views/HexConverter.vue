418d5583e33e63b78ead7b58bb58cb22e553c281
TheDream3r <patrick.raum@gmail.com>
2024-12-20 15:11:43 +0100

    Added the HexConverter view

---
+++ src/views/HexConverter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="HexConverter">
+      <h1>This is the hex converter</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .HexConverter {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
